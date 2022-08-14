e248f4486ac3a07cf1bdfe09031297140442c0fe
mehmet02 <mehmetekinci3402@hotmail.com>
2022-08-14 06:10:51 +0300

    initial commit Yeni VS Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductComponent } from './components/product/product.component';
+import { BasketComponent } from './components/basket/basket.component';
+import { PaymentComponent } from './components/payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ProductComponent,
+    BasketComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
